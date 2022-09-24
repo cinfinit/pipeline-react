7dfb43d52e8ac45aabd236b8dfb31b8d3c0ebe0b
INFINITE <infinite@192.168.1.5>
2022-09-25 01:50:43 +0530

    removed actions

--- src/App.js
+++ src/App.js
@@ -7,8 +7,9 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Branch 2 
+          SUPER SAMPLE
         </p>
+        <p>from new branch</p>
         <a
           className="App-link"
           href="https://reactjs.org"
